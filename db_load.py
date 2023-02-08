08ac84584cd99e504eb69e10349662aa554905fd
Adrien Riguet <adrien.riguet@hotmail.com>
2023-02-08 11:35:01 +0100

    mydbconnection

--- db_load.py
+++ db_load.py
@@ -1,13 +1,13 @@
 import mydbconnection as mdbcon
 import sqlalchemy
 # Connect to the database using an SSH tunnel
-conn = mdbcon.connect_to_db('C:\\Users\\tarik\\OneDrive\\Documents\\Formation_Dév_IA\\Projet_Netfloox\\config.ini', 'DB_NETFLOOX_MAT', ssh=False, local_port=3308, remote_port=3306,section='mysql_azure_netfloox', ssh_section='ssh_tunnel')
+conn = mdbcon.connect_to_db('config.yaml', 'mysql_azure_netfloox')
 
 # # Connect to the database directly
 # conn = mdbcon.connect_to_db('config.ini', 'mydb', ssh=False)
 
 res = conn.execute('''
- SHOW DATABASES;
+ CREATE TABLE netfloox_db.``;
  '''
 )
 
--- mydbconnection.py
+++ mydbconnection.py
@@ -1,63 +1,22 @@
-import configparser
-import os
+import yaml
 import paramiko
-import psycopg2
-import pymongo
 from sqlalchemy import create_engine
 
-def connect_to_db(config_file, db_name, ssh=False, local_port=None, remote_port=None,section=None, ssh_section=None):
+
+def connect_to_db(config_file, section, ssh=False, local_port=None, ssh_section=None):
     # Read configuration information from file
-    config = configparser.ConfigParser()
-    config.read(config_file)
-    section = section
-    user = config[section]['user']
-    password = config[section]['password']
-    host = config[section]['host']
-    port = config[section].getint('port')
-    if remote_port:
-        port = remote_port
-    db_type = config[section]['type']
-    
+    config = yaml.safe_load(config_file)
+
     if ssh:
-        # Read configuration information from file
-        config = configparser.ConfigParser()
-        config.read(config_file)
-        ssh_section = ssh_section
-        user_ssh = config[ssh_section]['user']
-        password_ssh = config[ssh_section]['password']
-        host_ssh = config[ssh_section]['host']
-        port_ssh = config[ssh_section].getint('port')
+        ssh_config = config[ssh_section]['user']
         # Connect to database using SSH tunnel
         ssh = paramiko.SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(host_ssh, username=user_ssh, password=password_ssh)
+        ssh.connect(ssh_config['host'], ssh_config['port'], ssh_config['user'], ssh_config['password'])
 
-        # Forward a local port to the remote database server
-        transport = ssh.get_transport()
-        dest_addr = (host, port)
         if local_port:
-            local_port = local_port
-        else:
-            local_port = port
-        local_addr = ('localhost', local_port)
-        channel = transport.open_channel("direct-tcpip", dest_addr, local_addr)
+            config[section]['port'] = local_port
 
-        host = host
-        port = local_port
-    
-    if db_type == 'postgresql':
-        # Connect to PostgreSQL database using SQLAlchemy
-        engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db_name}')
-        conn = engine.connect()
-        return conn
-    elif db_type == 'mysql':
-        # Connect to MySQL database using SQLAlchemy
-        engine = create_engine(f'mysql://{user}:{password}@{host}:{port}/{db_name}')
-        conn = engine.connect()
-        return conn
-    elif db_type == 'mongodb':
-        # Connect to MongoDB database
-        client = pymongo.MongoClient(f'mongodb://{user}:{password}@{host}:{port}/')
-        return client[db_name]
-    else:
-        raise ValueError(f"Unsupported database type: {db_type}")
+    # Connect to database using SQLAlchemy
+    engine = create_engine('{type}://{user}:{password}@{host}:{port}/{db_name}'.format(**config[section]))
+    return engine.connect()
