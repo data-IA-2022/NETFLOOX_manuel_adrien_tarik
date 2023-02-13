72b3fe85c2363eac03d9eba472718befa10d68a1
adrienRiguet <adrien.riguet@hotmail.com>
2023-02-13 10:33:27 +0100

    db_load debug

--- db_load.py
+++ db_load.py
@@ -13,14 +13,14 @@ import yaml
 def convert_dtype(types: dict) -> dict:
 
     conv = {
-        'VARCHAR': SQLAT.VARCHAR,
+        'VARCHAR':  SQLAT.VARCHAR,
         'SMALLINT': SQLAT.SMALLINT,
-        'BOOLEAN': SQLAT.BOOLEAN,
-        'INTEGER': SQLAT.INTEGER,
-        'FLOAT': SQLAT.FLOAT
+        'BOOLEAN':  SQLAT.BOOLEAN,
+        'INTEGER':  SQLAT.INTEGER,
+        'FLOAT':    SQLAT.FLOAT
     }
 
-    return {k: conv[v]() if len(v.split()) == 1 else conv[v.split()[0]](v.split(maxsplit=1)[1]) for k, v in types.items()}
+    return {k: conv[v]() if len(v.split()) == 1 else conv[v.split()[0]](int(v.split(maxsplit=1)[1])) for k, v in types.items()}
 
 
 ##### Chargement des configs
@@ -58,7 +58,7 @@ for tn in table_names:
 
     nb_chunks = ceil(nb_lines / ch_size)
 
-    dtype = convert_dtype(current_file['dtype'])
+    column = convert_dtype(current_file['column'])
 
     with pd.read_csv(path, sep='\t', na_values=current_file['na_values'], quoting=3, low_memory=False, chunksize=ch_size) as chunk_it:
 
@@ -66,6 +66,8 @@ for tn in table_names:
 
         n = 0
         for df in chunk_it:
+
+            lines_time = time.time()
 
             if 'explode_on' in current_file:
 
@@ -77,14 +79,16 @@ for tn in table_names:
 
                 df = df.explode(new_name).dropna()
 
-            df = df[list(dtype.keys())]
+            df = df[list(column.keys())]
 
-            df.to_sql(tn, conn, dtype = dtype, if_exists=if_ex, index=False)
+            df.to_sql(tn, conn, dtype = column, if_exists=if_ex, index=False)
 
             if_ex = 'append'
 
             n += 1
-            print(f" - {n_file:2}/{len(table_names)} - {tn} : {n:5,} / {nb_chunks:,}  -  {n/nb_chunks:8.4%}")
+            d = time.time() - file_time
+            d = f"{int(d):2} s" if d < 60 else f"{d:5.1} m"
+            print(f" - {n_file:2}/{len(table_names)} - {tn} : {n:5,} / {nb_chunks:,}  -  {n/nb_chunks:8.3%}  -  {d}")
 
     d = time.time() - file_time
     m = int(d/60)
