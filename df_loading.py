25ddddcf00843ef1a6c1c681db15b49bac6ffa96
Adrien Riguet <adrien.riguet@hotmail.com>
2023-02-23 11:43:23 +0100

    reso error

--- df_loading.py
+++ df_loading.py
@@ -2,17 +2,15 @@ import pandas as pd
 from db_connection import connect_to_db
 import yaml
 
-# Créer une connexion SQLAlchemy
-config = yaml.safe_load(open("..\\config_db_load.yaml", 'r'))
-engine = connect_to_db(config['param']['connection_config_file'], 'mysql_azure_netfloox_Ex')
+conn = connect_to_db('../config.yaml', 'mysql_azure_netfloox')
 
 datasets = {'tables' : {'films' : pd.read_pickle('./projet_netfloox/dataset_prédiction.pickle')},
-            'analyses': {'notes' : pd.read_sql_table('table_distrib_notes', engine),
-                         'types' : pd.read_sql_table('table_distrib_types', engine),
-                         'films_votes_rating_annee' : pd.read_sql_table('table_list_films_ex', engine),
-                         'decenie_votes_rating' : pd.read_sql_table('table_decenie_votes_rating', engine),
-                         'prop_notes_null' : pd.read_sql_table('table_prop_notes_null', engine),
-                         'region_diffusion_films' : pd.read_sql_table('table_region_len2_diffusion_films', engine),
-                         'classement_regions_production_films' : pd.read_sql_table('vue_classement_regions_production_films', engine),
-                         'region_len2_stats_films' : pd.read_sql_table('table_region_len2_stats_films', engine),
-                         'region_stats_films' : pd.read_sql_table('table_region_stats_films', engine)}}
+            'analyses': {'notes' : pd.read_sql_table('table_distrib_notes', conn),
+                         'types' : pd.read_sql_table('table_distrib_types', conn),
+                         'films_votes_rating_annee' : pd.read_sql_table('table_list_films_ex', conn),
+                         'decenie_votes_rating' : pd.read_sql_table('table_decenie_votes_rating', conn),
+                         'prop_notes_null' : pd.read_sql_table('table_prop_notes_null', conn),
+                         'region_diffusion_films' : pd.read_sql_table('table_region_len2_diffusion_films', conn),
+                         'classement_regions_production_films' : pd.read_sql_table('vue_classement_regions_production_films', conn),
+                         'region_len2_stats_films' : pd.read_sql_table('table_region_len2_stats_films', conn),
+                         'region_stats_films' : pd.read_sql_table('table_region_stats_films', conn)}}
